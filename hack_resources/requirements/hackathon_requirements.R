a730ea36f91da9cf8c445f46535c7a1fdb3e8bfc
Quinten-Wolf <quinten.wolf@eratos.com>
2024-03-19 09:53:36 +1100

    hackathon_upgrade_test

---
+++ hack_resources/requirements/hackathon_requirements.R
@@ -0,0 +1,27 @@
+# Requirements file for R environment
+
+# Data cleansing
+install.packages("tidyverse")
+
+# Data transformation
+install.packages("dplyr")
+install.packages("data.table")
+
+# Machine learning
+install.packages("mlr")
+
+# Dashboarding
+install.packages("shiny")
+
+# Mapbox interface
+install.packages("mapdeck")
+
+# Data visualisation
+install.packages("ggplot2")
+install.packages("ggmap")
+
+# Read Geospatial data
+install.packages("sf")
+
+# Maps
+install.packages("leaflet")
